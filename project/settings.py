fff0ee0520ddb9b2aa5f883d200da4b82740d4d2
Amr Namoura <amr.namora@gmail.com>
2026-02-28 18:06:53 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 AUTH_USER_MODEL = 'account.CustomUser'
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-key-for-dev-only')
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'True'
+# DEBUG = os.getenv('DEBUG', 'False').lower() == 'True'
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ['192.168.93.134','192.168.1.101','127.0.0.1','192.168.92.59','https://solar-system-repositories-management.onrender.com']
 
